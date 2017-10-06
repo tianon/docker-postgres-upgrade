57f4fca2acf009e4e9d7dd5843e1018bc8a716e1
Manuel Mendez <mmendez534@gmail.com>
2017-10-06 11:28:46 -0400

    add 10

--- update.sh
+++ update.sh
@@ -9,14 +9,15 @@ supportedVersions=(
 	9.4
 	9.5
 	9.6
+	10
 )
 
 for i in "${!supportedVersions[@]}"; do
 	old="${supportedVersions[$i]}"
-	docker pull "postgres:$old" > /dev/null
+	docker pull "postgres:$old" >/dev/null
 	oldVersion="$(docker run --rm "postgres:$old" sh -c 'echo $PG_VERSION')"
 	echo "# $old ($oldVersion)"
-	(( j = i + 1 ))
+	((j = i + 1))
 	for new in "${supportedVersions[@]:$j}"; do
 		dir="$old-to-$new"
 		echo "$old -> $new ($dir)"
@@ -26,7 +27,7 @@ for i in "${!supportedVersions[@]}"; do
 			-e "s!%%POSTGRES_OLD_VERSION%%!$oldVersion!g" \
 			-e "s!%%POSTGRES_NEW%%!$new!g" \
 			Dockerfile.template \
-			> "$dir/Dockerfile"
+			>"$dir/Dockerfile"
 		cp docker-upgrade "$dir/"
 	done
 done
